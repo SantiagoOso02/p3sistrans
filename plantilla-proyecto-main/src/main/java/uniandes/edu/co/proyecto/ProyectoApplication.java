1e09fd878807e45340d191fcbe6a61508cc5bb19
Daniela Torres <d.torrest2@uniandes.edu.co>
2024-05-23 12:26:27 -0500

    config app principal

--- plantilla-proyecto-main/src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ plantilla-proyecto-main/src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -1,13 +1,32 @@
 package uniandes.edu.co.proyecto;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import uniandes.edu.co.proyecto.repositorio.OficinaRepositorio;
+
+
+@ComponentScan({"uniandes.edu.co.proyecto.repositorio"})
 @SpringBootApplication
-public class ProyectoApplication {
+public class ProyectoApplication implements CommandLineRunner{
+
+	@Autowired
+	private OficinaRepositorio oficinaRepositorio;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception{
+
+		
 	}
 
 }
